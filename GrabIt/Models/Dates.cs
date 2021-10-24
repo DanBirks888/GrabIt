7448f7a0d5d79d1eb11babfe68bc56e44cd3d17e
DanBirks888 <DanBirks888@gmail.com>
2021-10-24 11:43:15 +0100

    Adding Date Ordinal To The End of a Date Number

---
+++ GrabIt/Models/Dates.cs
@@ -0,0 +1,21 @@
+﻿namespace GrabIt.Models
+{
+    public class Dates
+    {
+        public static string GetDateOrdinal(string date) {
+
+            if (date.EndsWith("11") || date.EndsWith("12") || date.EndsWith("13")) return $"{date}th";
+            
+            if (date.EndsWith("1")) return $"{date}st";
+
+            if (date.EndsWith("2")) return $"{date}nd";
+
+            if (date.EndsWith("3")) return $"{date}rd";
+
+            return $"{date}th";
+
+        }
+        
+        
+    }
+} 
--- GrabIt/Models/URLs.cs
+++ GrabIt/Models/URLs.cs
@@ -7,12 +7,10 @@ namespace GrabIt.Models
     {
         public string LinkShaver(string input)
         {
-            // If it has more than one dot
             if(new Regex("(\\..*){2}").Match(input).Success)
             {
                 return new Regex("^(.*?)\\.").Replace(input, "");
             }
-            // If it doesnt not have multiple dots
             else
             {
                 return new Regex("(https:\\/\\/www|http:\\/\\/www|https:\\/\\/|http:\\/\\/|https\\/\\/|http\\/\\/|www)").Replace(input, "");
